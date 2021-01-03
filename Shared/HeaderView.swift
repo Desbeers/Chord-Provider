1c90be04fbae7733e1c4f379a3e3d13d92d56d7d
Nick Berendsen <nick@desbeers.nl>
2021-01-03 16:37:22 +0100

    Spit HeaderView for small iOS devices
    Show details below title when it's shown on a small screen.

--- Shared/HeaderView.swift
+++ Shared/HeaderView.swift
@@ -6,35 +6,79 @@ import SwiftUI
 
 struct HeaderView: View {
     var song: Song
+    #if os(iOS)
+    @Environment(\.horizontalSizeClass) var sizeClass
+    #endif
 
     var body: some View {
+        #if os(macOS)
             HStack(alignment: .center) {
                 Spacer()
-                if song.artist != nil {
-                    Text(song.artist!).font(.headline)
-                }
-                if song.title != nil {
-                    Text(song.title!)
-                }
-                if song.key != nil {
-                    Label(song.key!, systemImage: "key").padding(.leading)
-                }
-                if song.capo != nil {
-                    Label(song.capo!, systemImage: "paperclip").padding(.leading)
-                }
-                if song.tempo != nil {
-                    Label(song.tempo!, systemImage: "metronome").padding(.leading)
-                }
-                if song.time != nil {
-                    Label(song.time!, systemImage: "repeat").padding(.leading)
-                }
-                #if os(macOS)
+                HeaderViewGeneral(song: song)
+                HeaderViewDetails(song: song)
                 if song.musicpath != nil {
                     AudioPlayer(song: song)
                 }
-                #endif
                 Spacer()
             }
             .padding(4)
+        #endif
+        #if os(iOS)
+        if sizeClass == .compact {
+            VStack {
+                HStack {
+                    Spacer()
+                    HeaderViewGeneral(song: song).padding(4)
+                    Spacer()
+                }
+                HeaderViewDetails(song: song)
+            }
+            .padding(4)
+        } else {
+            HStack(alignment: .center) {
+                Spacer()
+                HeaderViewGeneral(song: song)
+                HeaderViewDetails(song: song)
+                Spacer()
+            }
+            .padding(4)
+        }
+        #endif
+    }
+}
+
+struct HeaderViewGeneral: View {
+    var song: Song
+
+    var body: some View {
+        HStack(alignment: .center) {
+            if song.artist != nil {
+                Text(song.artist!).font(.headline)
+            }
+            if song.title != nil {
+                Text(song.title!)
+            }
+        }
+    }
+}
+
+struct HeaderViewDetails: View {
+    var song: Song
+
+    var body: some View {
+        HStack(alignment: .center) {
+            if song.key != nil {
+                Label(song.key!, systemImage: "key").padding(.leading)
+            }
+            if song.capo != nil {
+                Label(song.capo!, systemImage: "paperclip").padding(.leading)
+            }
+            if song.tempo != nil {
+                Label(song.tempo!, systemImage: "metronome").padding(.leading)
+            }
+            if song.time != nil {
+                Label(song.time!, systemImage: "repeat").padding(.leading)
+            }
+        }
     }
 }
