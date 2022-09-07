0cff06649cc275ebb6341ed267f5765ab6d4d010
Nick Berendsen <58348935+Desbeers@users.noreply.github.com>
2022-09-07 14:47:12 +0200

    Cleanup Quick Look extension

--- Chord Provider/Parser/Song.swift
+++ Chord Provider/Parser/Song.swift
@@ -24,32 +24,3 @@ struct Song {
     var sections = [Song.Section]()
     var chords = [Chord]()
 }
-
-//let buildSongDebouncer = Debouncer(duration: 1)
-/// Update the song item
-struct SongViewModifier: ViewModifier {
-
-    @Binding var document: ChordProDocument
-    @Binding var song: Song
-    let file: URL?
-
-    @SceneStorage("showEditor") var showEditor: Bool = false
-
-    func body(content: Content) -> some View {
-        content
-            .task {
-                /// Always open the editor for a new file
-                if document.text == ChordProDocument.newText {
-                    showEditor = true
-                }
-                song = ChordPro.parse(text: document.text, file: file ?? nil)
-            }
-            .onChange(of: document.text) { _ in
-                Task {
-                    await document.buildSongDebouncer.submit {
-                        song = ChordPro.parse(text: document.text, file: file ?? nil)
-                    }
-                }
-            }
-    }
-}
--- Chord Provider/SharedViews/SongView.swift
+++ Chord Provider/SharedViews/SongView.swift
@@ -23,3 +23,31 @@ struct SongView: View {
         }
     }
 }
+
+/// Update the song item
+struct SongViewModifier: ViewModifier {
+
+    @Binding var document: ChordProDocument
+    @Binding var song: Song
+    let file: URL?
+
+    @SceneStorage("showEditor") var showEditor: Bool = false
+
+    func body(content: Content) -> some View {
+        content
+            .task {
+                /// Always open the editor for a new file
+                if document.text == ChordProDocument.newText {
+                    showEditor = true
+                }
+                song = ChordPro.parse(text: document.text, file: file ?? nil)
+            }
+            .onChange(of: document.text) { _ in
+                Task {
+                    await document.buildSongDebouncer.submit {
+                        song = ChordPro.parse(text: document.text, file: file ?? nil)
+                    }
+                }
+            }
+    }
+}
