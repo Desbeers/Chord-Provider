6385f4b14165104fb9fc5e58bcbca4ff05b59673
Nick Berendsen <58348935+Desbeers@users.noreply.github.com>
2023-07-15 22:40:36 +0200

    Make `ExportSong` compile on iOS
    Work in progress, it is not exporting yet...

--- Chord Provider/macOS/General/ExportSong.swift
+++ Chord Provider/macOS/General/ExportSong.swift
@@ -33,143 +33,21 @@ enum ExportSong {
         return nil
     }
 
-    /// Render the header of the song
-    /// - Parameter song: The song
-    /// - Returns: The header as CGImage
-    @MainActor
-    private static func renderHeader(song: Song) -> CGImage? {
-        let renderer = ImageRenderer(
-            content:
-                VStack {
-                    Text(song.title ?? "Title")
-                        .font(.largeTitle)
-                    Text(song.artist ?? "Artist")
-                        .font(.title2)
-                        .foregroundColor(Color("AccentColor"))
-                        .padding(.bottom)
-                    HeaderView.Details(song: song)
-                }
-                .frame(width: pageWidth, alignment: .center)
-                .preferredColorScheme(.light)
-                .background(.white)
-        )
-        renderer.scale = rendererScale
-        return renderer.cgImage
-    }
-
-    /// Render the chords of the song
-    /// - Parameter song: The song
-    /// - Returns: The header as CGImage
-    @MainActor
-    private static func renderChords(song: Song) -> CGImage? {
-        /// Size of the chord diagram
-        var frame: CGRect {
-            var width = Int((pageWidth * 2.0) / Double(song.chords.count))
-            width = width > Int(pageWidth / 4.0) ? Int(pageWidth / 4.0) : width
-            let height = Int(Double(width) * 1.5)
-            return CGRect(x: 0, y: 0, width: width, height: height)
-        }
-        /// Render the chords
-        let renderer = ImageRenderer(
-            content:
-                HStack {
-                    ForEach(song.chords.sorted(using: KeyPathComparator(\.name))) { chord in
-                        VStack {
-                            Text(chord.display)
-                                .foregroundColor(Color("AccentColor"))
-                                .font(.caption)
-                            let layer = chord.chordPosition.chordLayer(
-                                rect: frame,
-                                showFingers: false,
-                                chordName: .init(show: false)
-                            )
-                            if let image = layer.image() {
-                                Image(swiftImage: image)
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                    .frame(width: frame.width / 3)
-                            }
-                        }
-                    }
-                }
-                .padding()
-                .frame(width: pageWidth, alignment: .center)
-                .preferredColorScheme(.light)
-                .background(.white)
-        )
-        renderer.scale = rendererScale
-        return renderer.cgImage
-    }
-
-    /// Render all the parts of the song
-    /// - Parameter song: The song
-    /// - Returns: An array of `CGImage`
-    @MainActor
-    private static func renderParts(song: Song) -> [CGImage] {
-        var parts: [CGImage] = []
-        var part: CGImage?
-        for section in song.sections {
-            switch section.type {
-            case .verse:
-                part = renderPart(view: Song.Render.VerseView(section: section, scale: 1))
-            case .chorus:
-                part = renderPart(view: Song.Render.ChorusView(section: section, scale: 1))
-            case .bridge:
-                part = renderPart(view: Song.Render.VerseView(section: section, scale: 1))
-            case .repeatChorus:
-                part = renderPart(view: Song.Render.RepeatChorusView(section: section, scale: 1))
-            case .tab:
-                part = renderPart(view: Song.Render.TabView(section: section, scale: 1))
-            case .grid:
-                part = renderPart(view: Song.Render.GridView(section: section, scale: 1))
-            case .comment:
-                part = renderPart(view: Song.Render.CommentView(section: section, scale: 1))
-            default:
-                part = renderPart(view: Song.Render.PlainView(section: section, scale: 1))
-            }
-            if let part {
-                parts.append(part)
-            }
-        }
-        return parts
-
-        /// Helper function to render a part
-        /// - Parameter view: The SwiftUI View to render
-        /// - Returns: A `CGImage` of the View
-        func renderPart<T: View>(view: T) -> CGImage? {
-            let renderer = ImageRenderer(
-                content:
-                    VStack {
-                        Grid(alignment: .topTrailing, verticalSpacing: 20) {
-                            view
-                        }
-                        .padding()
-                    }
-                    .frame(width: pageWidth, alignment: .leading)
-                    .accentColor(Color("AccentColor"))
-                    .background(.white)
-                    .font(.system(size: 14))
-            )
-            renderer.scale = rendererScale
-            return renderer.cgImage
-        }
-    }
-
     /// Merge all the parts into pages
     /// - Parameters:
     ///   - header: The header of the song
     ///   - parts: The parts of the song
     /// - Returns: An array of `NSImage`
-    static private func mergeParts(header: CGImage, parts: [CGImage]) -> [NSImage] {
+    static private func mergeParts(header: CGImage, parts: [CGImage]) -> [SWIFTImage] {
 
         var page = CIImage(cgImage: header)
 
         /// A4 aspect ratio for a page
-        let pageSize = NSSize(width: page.extent.width / rendererScale, height: (page.extent.width * 1.414) / rendererScale)
+        let pageSize = CGSize(width: page.extent.width / rendererScale, height: (page.extent.width * 1.414) / rendererScale)
 
         var pageHeight: Double = page.extent.size.height
 
-        var pages: [NSImage] = []
+        var pages: [SWIFTImage] = []
 
         guard
             let filter = CIFilter(name: "CIAdditionCompositing")
@@ -211,31 +89,33 @@ enum ExportSong {
                 width: page.extent.width / rendererScale,
                 height: page.extent.height / rendererScale
             )
+#if os(macOS)
             let rep = NSCIImageRep(ciImage: page)
 
-            let finalA4 = NSImage(size: pageSize)
+            let finalA4 = SWIFTImage(size: pageSize)
             finalA4.lockFocus()
 
             rep.draw(in: rectToDrawIn)
             finalA4.unlockFocus()
 
             pages.append(finalA4)
+#endif
         }
     }
 
     /// Create a PDF from the pages
     /// - Parameter pages: The pages of the song
     /// - Returns: A PDF as `NSData`
-    static private func pages2pdf(pages: [NSImage]) -> NSData? {
+    static private func pages2pdf(pages: [SWIFTImage]) -> NSData? {
 
         /// The margins for the individual page
-        let margin = NSSize(width: 10, height: 14.14)
+        let margin = CGSize(width: 10, height: 14.14)
         /// The content size for PDF page, in A4 ratio
-        let contentSize = NSSize(width: pages.first?.size.width ?? 0, height: pages.first?.size.height ?? 0)
+        let contentSize = CGSize(width: pages.first?.size.width ?? 0, height: pages.first?.size.height ?? 0)
         /// The box dimensions of the PDF
-        var mediaBox = NSRect(x: 0, y: 0, width: contentSize.width + (margin.width * 2), height: contentSize.height + (margin.height * 2))
+        var mediaBox = CGRect(x: 0, y: 0, width: contentSize.width + (margin.width * 2), height: contentSize.height + (margin.height * 2))
         /// The box dimensions of the content
-        let contentBox = NSRect(x: margin.width, y: margin.height, width: contentSize.width, height: contentSize.height)
+        let contentBox = CGRect(x: margin.width, y: margin.height, width: contentSize.width, height: contentSize.height)
 
         /// Create the PDF
         let pdfData = NSMutableData()
@@ -248,7 +128,8 @@ enum ExportSong {
         /// Add all the pages````
         for page in pages {
             /// Convert NSImage to CGImage
-            var rect = NSRect(origin: CGPoint(x: 0, y: 0), size: page.size)
+            var rect = CGRect(origin: CGPoint(x: 0, y: 0), size: page.size)
+#if os(macOS)
             guard
                 let cgPage = page.cgImage(forProposedRect: &rect, context: NSGraphicsContext.current, hints: nil)
             else {
@@ -258,6 +139,9 @@ enum ExportSong {
             pdfContext.beginPage(mediaBox: &mediaBox)
             pdfContext.draw(cgPage, in: contentBox)
             pdfContext.endPage()
+#else
+            return nil
+#endif
         }
         return pdfData
     }
