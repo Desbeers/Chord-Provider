65d9e32d1638c2c660bd6bc0f3b8f0c88a0491de
Nick Berendsen <58348935+Desbeers@users.noreply.github.com>
2023-09-29 16:47:03 +0200

    Try to fix 'Pinch to Zoom'
    Still a bit buggy...

--- Chord Provider/Views/SongView.swift
+++ Chord Provider/Views/SongView.swift
@@ -13,16 +13,6 @@ struct SongView: View {
     let song: Song
     /// The scale factor of the `View`
     @SceneStorage("scale") var scale: Double = 1.2
-    /// Pinch to zoom gesture
-    var magnificationGesture: some Gesture {
-        MagnificationGesture()
-            .onChanged { value in
-                let newScale = scale * value
-                if 0.8...2.0 ~= newScale {
-                    scale = newScale
-                }
-            }
-    }
     /// The body of the `View`
     var body: some View {
         VStack {
@@ -31,22 +21,17 @@ struct SongView: View {
                     Song.Render(song: song, scale: scale, style: .asGrid)
                     Song.Render(song: song, scale: scale, style: .asList)
                 }
-                    .frame(maxWidth: .infinity, maxHeight: .infinity)
-#if os(visionOS)
-                    .toolbar {
-                        ToolbarItemGroup(placement: .topBarTrailing) {
-                            Image(systemName: "magnifyingglass")
-                            ToolbarView.ScaleSlider()
-                                .frame(width: 100)
-                        }
-                    }
-#else
-                    .gesture(magnificationGesture)
-#endif
-
-                    .padding()
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .padding()
             }
         }
+        .gesture(
+            MagnificationGesture()
+                .onChanged { scale in
+                    let newScale = self.scale * scale
+                    self.scale = min(max(newScale.magnitude, 0.8), 2.0)
+                }
+        )
         .frame(maxWidth: .infinity, maxHeight: .infinity)
     }
 }
