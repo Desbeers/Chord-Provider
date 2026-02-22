2c2e761aa9c457d455757a2e93d0b2fdd970ff36
Nick Berendsen <58348935+Desbeers@users.noreply.github.com>
2026-02-22 06:59:43 +0100

    (Gnome) Pinch to zoom the song

--- ChordProviderGnome/Sources/ChordProvider/GtkRender/GtkRender+PageView.swift
+++ ChordProviderGnome/Sources/ChordProvider/GtkRender/GtkRender+PageView.swift
@@ -8,6 +8,7 @@
 import Foundation
 import Adwaita
 import ChordProviderCore
+import CAdw
 
 extension GtkRender {
 
@@ -36,6 +37,30 @@ extension GtkRender {
                 }
             }
             .hexpand()
+            .inspectOnAppear { storage in
+
+                // MARK: Pinch to Zoom
+
+                let gesture = gtk_gesture_zoom_new()
+                var zoomStart = appState.settings.theme.zoom
+                /// *begin* signal
+                storage.connectSignal(name: "begin", argCount: 1, pointer: gesture) { _ in
+                    zoomStart = appState.settings.theme.zoom
+                }
+                /// *scale-changed* signal
+                storage.connectSignal(name: "scale-changed", pointer: gesture) {
+                    let delta = gtk_gesture_zoom_get_scale_delta(gesture)
+                    let newZoom = zoomStart * delta
+                    let step: Double = 0.05
+                    let stepped = (newZoom / step).rounded() * step
+                    if stepped != appState.settings.theme.zoom {
+                        Idle {
+                            appState.settings.theme.zoom = min(max(stepped, 0.5), 2.0)
+                        }
+                    }
+                }
+                gtk_widget_add_controller(storage.opaquePointer?.cast(), gesture)
+            }
         }
         /// The sections
         @ViewBuilder var sections: Body {
