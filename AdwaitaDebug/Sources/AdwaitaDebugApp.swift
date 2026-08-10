8013978a7517af3ab50fca249206d8b5e0d53b27
Nick Berendsen <58348935+Desbeers@users.noreply.github.com>
2026-08-10 17:46:36 +0200

    Update debug application

--- AdwaitaDebug/Sources/AdwaitaDebugApp.swift
+++ AdwaitaDebug/Sources/AdwaitaDebugApp.swift
@@ -16,5 +16,29 @@ struct AdwaitaDebugApp: App {
                         .empty()
                 }
         }
+        .title("Orientation, Expanding & Alignment")
+        .defaultSize(width: 800, height: 600)
+    }
+}
+
+enum Tab: String, CaseIterable {
+    case separator
+    case forEach
+    case either
+    case switching
+}
+
+enum Filler: String {
+    case left
+    case center
+    case right
+
+    case one
+    case two
+    case three
+    case four
+
+    static var numbers: [Filler] {
+        [.one, .two, .three, .four]
     }
 }
--- AdwaitaDebug/Sources/Main.swift
+++ AdwaitaDebug/Sources/Main.swift
@@ -4,106 +4,207 @@ import Adwaita
 /// The main `View` for the application
 struct Main: View {
 
-    @State private var appState = AppState()
+    @State var selectedTab: Tab = .separator
 
-        /// A signal to open a file
-        @State private var openFile = Signal()
-        /// Show a toast
-        @State private var showToast = Signal()
-        /// The toast message
-        @State private var toastMessage: String = ""
+    @State var eitherToggle: Bool = false
 
     var view: Body {
         VStack {
-            HStack {
-                // NESTED
-                VStack {
-                    Text("NESTED")
-                        .title1()
-                    Button("Toast me") {
-                        appState.scene.toastMessage = Date.now.formatted()
-                        appState.scene.showToast.signal()
-                    }
-                    .halign(.center)
-                    .padding()
-                    Button("Open file") {
-                        appState.scene.openFile.signal()
-                    }
-                    .halign(.center)
-                    .padding()
-                    Text(helpText)
-                }
-                .padding()
-                .hexpand()
-                // The **Toast** message
-                .toast(
-                    appState.scene.toastMessage,
-                    signal: $appState.scene.showToast
-                )
-                .fileImporter(open: $appState.scene.openFile) { url in
-                    appState.scene.toastMessage = url.lastPathComponent
-                    appState.scene.showToast.signal()
-                }
-                Separator()
-                // NOT NESTED
-                VStack {
-                    Text("NOT NESTED")
-                        .title1()
-                    Button("Toast me") {
-                        toastMessage = Date.now.formatted()
-                        showToast.signal()
-                    }
-                    .halign(.center)
-                    .padding()
-                    Button("Open file") {
-                        openFile.signal()
-                    }
-                    .halign(.center)
-                    .padding()
-                    Text("/// Normal, no problems")
-                }
-                .padding()
-                .hexpand()
-                // The **Toast** message
-                .toast(
-                    toastMessage,
-                    signal: $showToast
-                )
-                .fileImporter(open: $openFile) { url in
-                    toastMessage = url.lastPathComponent
-                    showToast.signal()
+            ToggleGroup(selection: $selectedTab, values: Tab.allCases, id: \.self, label: \.rawValue)
+            ScrollView {
+                switch selectedTab {
+                case .separator:
+                    separators
+                case .forEach:
+                    foreach
+                case .either:
+                    either
+                case .switching:
+                    switching
                 }
             }
+            .vexpand()
+            .hexpand()
         }
     }
-}
 
-/// Appstate
-struct AppState {
-    var scene = Scene()
-}
+    var separators: AnyView {
+        VStack(spacing: 10) {
+            Text("Separators know their orientation from their parent")
+                .accent()
+            Text("In VStack")
+                .title2()
+            VStack(spacing: 5) {
+                Text(Filler.left.rawValue)
+                Separator()
+                Text(Filler.right.rawValue)
+            }
+            .padding()
+            .card()
+            status("This is OK, but only because its the default")
+                .accent()
+            Text("In HStack")
+                .title2()
+            HStack(spacing: 5) {
+                Text(Filler.left.rawValue)
+                Separator()
+                Text(Filler.right.rawValue)
+            }
+            .padding()
+            .card()
+            status("This is OK, but I expect all childeren the same width and centered")
+                .warning()
+            Text("In CenterBox")
+                .title2()
+            CenterBox()
+                .startWidget {
+                    Text(Filler.left.rawValue)
+                }
+                .centerWidget {
+                    Separator()
+                }
+                .endWidget {
+                    Text(Filler.right.rawValue)
+                }
+                .padding()
+                .card()
+            status("This is OK but needs a more <i>Swifty</i> init() and left and right should be centered")
+                .warning()
+        }
+    }
 
-extension AppState {
+    var foreach: AnyView {
+        VStack(spacing: 10) {
+            Text("ForEch does not know the orientation of its parent")
+                .error()
+             Text("In VStack")
+                .title2()
+            VStack(spacing: 5) {
+                ForEach(Filler.numbers, id: \.rawValue) { number in
+                    Text(number.rawValue)
+                }
+            }
+            .padding()
+            .card()
+            status("This is OK")
+                .accent()
+            Text("In HStack")
+                .title2()
+            VStack(spacing: 5) {
+                ForEach(Filler.numbers, id: \.rawValue) { number in
+                    Text(number.rawValue)
+                }
+            }
+            .padding()
+            .card()
+            status("This is not OK, I expect the elements horizontal but it will default to vertical and I have to set the orientation specific myself")
+                .error()
+        }
+    }
 
-    /// Scenestate
-    struct Scene {
-        /// A signal to open a file
-        var openFile = Signal()
-        /// Show a toast
-        var showToast = Signal()
-        /// The toast message
-        var toastMessage: String = ""
+    var either: AnyView {
+        VStack(spacing: 10) {
+            Text("EitherView does not know their orientation from their parent")
+                .error()
+            Toggle("Toggle Either", isOn: $eitherToggle)
+                .halign(.center)
+            Text("In VStack")
+                .title2()
+            VStack(spacing: 5) {
+                EitherView(
+                    eitherToggle,
+                    view1: {
+                        Text(Filler.left.rawValue)
+                        Separator()
+                        Text(Filler.right.rawValue)
+                    },
+                    else: {
+                        Text(Filler.one.rawValue)
+                        Separator()
+                        Text(Filler.two.rawValue)
+                    }
+                )
+                .padding()
+                .card()
+            }
+            status("This is OK")
+                .accent()
+            Text("In HStack")
+                .title2()
+            HStack(spacing: 5) {
+                EitherView(
+                    eitherToggle,
+                    view1: {
+                        Text(Filler.left.rawValue)
+                        Separator()
+                        Text(Filler.right.rawValue)
+                    },
+                    else: {
+                        Text(Filler.one.rawValue)
+                        Separator()
+                        Text(Filler.two.rawValue)
+                    }
+                )
+                .padding()
+                .card()
+            }
+            status("This is not OK, it should be aligned horizontal")
+                .error()
+            Text("Notice the different alignments?")
+                .title4()
+        }
+    }
+
+    var switching: AnyView {
+        VStack(spacing: 10) {
+            Text("Switch will reset the orientation of its parent")
+                .error()
+            Toggle("Toggle Switch", isOn: $eitherToggle)
+                .halign(.center)
+            Text("In VStack")
+                .title2()
+            VStack(spacing: 5) {
+                switch eitherToggle {
+                case true:
+                    Text(Filler.left.rawValue)
+                    Separator()
+                    Text(Filler.right.rawValue)
+                case false:
+                    Text(Filler.one.rawValue)
+                    Separator()
+                    Text(Filler.two.rawValue)
+                }
+            }
+            .padding()
+            .card()
+            status("This is OK")
+                .accent()
+            Text("In HStack")
+                .title2()
+            HStack(spacing: 5) {
+                switch eitherToggle {
+                case true:
+                    Text(Filler.left.rawValue)
+                    Separator()
+                    Text(Filler.right.rawValue)
+                case false:
+                    Text(Filler.one.rawValue)
+                    Separator()
+                    Text(Filler.two.rawValue)
+                }
+            }
+            .padding()
+            .card()
+            status("This is not OK, it should be aligned horizontal")
+                .error()
+            Text("Notice the different alignments?")
+                .title4()
+        }
+    }    
+
+    func status(_ text: String) -> AnyView {
+        Text(text)
+            .useMarkup()
+            .caption()
     }
 }
-
-let helpText: String = """
-    /// Signals repeats previous signals
-    ///
-    /// - Press toast button
-    /// - Press toast button again: it repeats itself twise
-    ///
-    /// - Press file button, select file
-    /// - Press toast button, file dialog pops-up again
-    ///
-    /// The signals are nested in the appState/Scene struct
-"""
